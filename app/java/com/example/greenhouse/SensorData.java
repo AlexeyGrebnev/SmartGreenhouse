8613fca7587658f37912aad3627308116b8636fe
Гребнев Алексей <gr3bneval@yandex.ru>
2025-04-17 22:56:38 +0500

    feat: create SensorData model / Создаёт Java-класс SensorData, в котором описана модель данных, получаемых с теплицы. Это включает температуру, влажность, освещённость и другие параметры

---
+++ app/java/com/example/greenhouse/SensorData.java
@@ -0,0 +1,11 @@
+package com.example.greenhouse;
+
+public class SensorData {
+    private float temperature;
+    private float humidity;
+    private float light;
+
+    public float getTemperature() { return temperature; }
+    public float getHumidity() { return humidity; }
+    public float getLight() { return light; }
+}
